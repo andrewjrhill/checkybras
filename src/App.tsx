57c9526313bae2a7a53b37733f6426d7567e1cab
Andrew Hill <3358672+andrewjrhill@users.noreply.github.com>
2021-05-11 00:39:02 +0200

    checky checkbras

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import uniqid from 'uniqid'
 import './App.css';
+import { Checkbox, Label } from 'theme-ui'
 
-function App() {
+interface DummyData {
+  id: string;
+  label: string;
+}
+
+const dummyData: DummyData[] = [
+  {id: uniqid(), label: 'Dobek'},
+  {id: uniqid(), label: 'Ruhur'},
+  {id: uniqid(), label: 'Obull'},
+  {id: uniqid(), label: 'Miomloh'},
+  {id: uniqid(), label: 'Knuorgeth'},
+  {id: uniqid(), label: 'Thrandaq'},
+]
+
+const App: React.FC = () => {
+  const [selectAllChecked, setSelectAllChecked] = useState<boolean>(false)
+  const [checkedCheckboxesIds, setCheckedCheckboxesIds] = useState<string[]>([])
+
+  const toggleSelectAll = () => {
+    if (selectAllChecked === true) {
+      setSelectAllChecked(false)
+      setCheckedCheckboxesIds([])
+    } else {
+      setSelectAllChecked(true)
+      setCheckedCheckboxesIds(dummyData.map(dat => dat.id))
+    }
+  }
+
+  const onChange = (data: DummyData) => {
+    if (checkedCheckboxesIds.includes(data.id)) {
+      setCheckedCheckboxesIds(checkedCheckboxesIds.filter(id => id !== data.id))
+    } else {
+      setCheckedCheckboxesIds([...checkedCheckboxesIds, data.id])
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header'>
+      <Label><Checkbox checked={selectAllChecked} onChange={() => toggleSelectAll()} />select all</Label>
+      <hr />
+      {dummyData.map((data, index) => <Label key={data.id}><Checkbox checked={checkedCheckboxesIds.includes(data.id)} onChange={() => onChange(data)}/>{data.label}</Label>)}
     </div>
   );
 }
